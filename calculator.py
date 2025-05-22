df3273f337e0538494fee5fb2b91ec8776f2a2a5
Bishayan21 <yalgaar556@gmail.com>
2025-05-22 19:26:51 +0530

    added extra lines

--- calculator.py
+++ calculator.py
@@ -3,3 +3,11 @@ b=int(input("Enter your second number :"))
 operation=input("Enter your operator :")
 if operation=='+':
     print(a+b)
+elif operator=='-':
+    print(a-b)
+elif operator=='*':
+    print(a*b)
+elif operator=='/':
+    print(a/b)
+else:
+    print("Invalid syntax")
